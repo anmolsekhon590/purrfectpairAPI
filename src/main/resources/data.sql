7a42a26eeb3656c74ac734af79e5499a4f3e172e
Anmol Sekhon <54895453+anmolsekhon590@users.noreply.github.com>
2021-05-28 15:15:46 -0400

    updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,6 @@ INSERT INTO PET VALUES(NULL, 'Tom', 'Cat', 'Male');
 INSERT INTO PET VALUES(NULL, 'Ben', 'Dog', 'Male');
 INSERT INTO PET VALUES(NULL, 'Pet3', 'Dog', 'Female');
 INSERT INTO PET VALUES(NULL, 'Pet4', 'Dog', 'Female');
+
+INSERT INTO USER VALUES(NULL, 'user1@gmail.com', 'password');
+INSERT INTO USER VALUES(NULL, 'user2@gmail.com', '12345678');
