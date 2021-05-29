8d5e83e77cf5b39730bb98819bb9b6c9500f546b
Anmol Sekhon <54895453+anmolsekhon590@users.noreply.github.com>
2021-05-29 14:37:04 -0400

    changed pet type

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO PET VALUES(NULL, 'Tom', 'Cat', 'Male');
-INSERT INTO PET VALUES(NULL, 'Ben', 'Dog', 'Male');
-INSERT INTO PET VALUES(NULL, 'Pet3', 'Dog', 'Female');
-INSERT INTO PET VALUES(NULL, 'Pet4', 'Dog', 'Female');
+INSERT INTO PET VALUES(NULL, 'Tom', 'cats', 'Male');
+INSERT INTO PET VALUES(NULL, 'Ben', 'dogs', 'Male');
+INSERT INTO PET VALUES(NULL, 'Pet3', 'cats', 'Female');
+INSERT INTO PET VALUES(NULL, 'Pet4', 'dogs', 'Female');
 
 INSERT INTO USER VALUES(NULL, 'user1@gmail.com', 'password');
 INSERT INTO USER VALUES(NULL, 'user2@gmail.com', '12345678');
