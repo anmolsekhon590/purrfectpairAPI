800144af7d0052e8ae7831ffe4c0b4032b3fd271
Anmol Sekhon <54895453+anmolsekhon590@users.noreply.github.com>
2021-05-29 22:42:53 -0400

    added more pets in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,8 @@ INSERT INTO PET VALUES(NULL, 1,'Jamie', 'dogs', 'Female', 'black', 'golden retri
 INSERT INTO PET VALUES(NULL, 1,'Lola', 'rabbit', 'male', 'brown', 'Eastern cottontail', 'adult', 'small');
 INSERT INTO PET VALUES(NULL, 1,'Flippy', 'tortoise', 'male', 'grey', 'Eastern Musk', 'senior', 'medium');
 INSERT INTO PET VALUES(NULL, 1,'Pierre', 'parrot', 'male', 'green', 'Eastern Musk', 'young', 'medium');
+INSERT INTO PET VALUES(NULL, 1,'Kartoos', 'dogs', 'male', 'white', 'Pitbull', 'adult', 'large');
+INSERT INTO PET VALUES(NULL, 2,'Cheeks', 'hamster', 'female', 'golden', 'Chinese Hamster', 'young', 'small');
 
 INSERT INTO USER VALUES(NULL, 'user1@gmail.com', 'password');
 INSERT INTO USER VALUES(NULL, 'user2@gmail.com', '12345678');
