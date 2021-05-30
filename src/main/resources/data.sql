90439af3d4618e8bded6e72f843a3e271514e254
Anmol Sekhon <54895453+anmolsekhon590@users.noreply.github.com>
2021-05-29 22:01:42 -0400

    updated names in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO PET VALUES(NULL, 1,'Tom', 'cats', 'Male', 'white', 'british shorthair', 'young', 'small');
 INSERT INTO PET VALUES(NULL, 2,'Ben', 'dogs', 'Male', 'brown', 'german shepherd', 'young', 'large');
-INSERT INTO PET VALUES(NULL, 2,'Pet3', 'cats', 'Female', 'grey', 'ragdoll', 'young', 'medium');
-INSERT INTO PET VALUES(NULL, 1,'Pet4', 'dogs', 'Female', 'black', 'golden retriever', 'senior', 'large');
+INSERT INTO PET VALUES(NULL, 2,'Callie', 'cats', 'Female', 'grey', 'ragdoll', 'young', 'medium');
+INSERT INTO PET VALUES(NULL, 1,'Jamie', 'dogs', 'Female', 'black', 'golden retriever', 'senior', 'large');
 
 INSERT INTO USER VALUES(NULL, 'user1@gmail.com', 'password');
 INSERT INTO USER VALUES(NULL, 'user2@gmail.com', '12345678');
